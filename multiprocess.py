be012d3655967713f3ebaa1f2cbafe328470338f
Renato dos Santos <renato.santos@wplex.com.br>
2018-08-28 13:20:10 -0300

    agora sim async

--- multiprocess.py
+++ multiprocess.py
@@ -14,15 +14,19 @@ pool = ThreadPool(processes=5)
 # do some other stuff in the main process
 clients = ['cliente1', 'cliente2', 'cliente3','cliente4','cliente5']
 theList = []
+asyncResults = []
 
 for client in clients:
+    asyncResult = pool.apply_async(getResult, args=(client,)) # tuple of args for foo
+    asyncResults.append(asyncResult) # just append
+
+for result in asyncResults:
     try:
-        asyncResult = pool.apply_async(getResult, args=(client,)) # tuple of args for foo
-        returnValue = asyncResult.get(timeout=1)  # get the return value from your function.
-        theList.append(returnValue) # just append
+        result = result.get(timeout=1)  # get the return value from your function.
+        theList.append(result) # just append
     except TimeoutError:
-        print("Client: " + client + " is Timeout") #time is over...
         theList.append("Timeout") # append again, but with timeout
+
 
 pool.close()
 pool.join()
